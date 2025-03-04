a57d8561cd24f1d3ff1d93002e7019b7d967d087
Selin <ozenselin@users.noreply.github.com>
2025-03-04 13:40:05 +0300

    Add: update current color function

--- script.js
+++ script.js
@@ -4,9 +4,23 @@ let size = 64;
 //select sketchpad
 const sketchpad = document.querySelector(".sketchpad");
 
+//color picker
+const colorPicker = document.querySelector("#range-color");
+
+//initially current color is gray
+let currentColor = "#AAAAAA";
+
 //create and append 64 boxes into the sketchpad
 for(let i = 0; i<size; i++){
     const box = document.createElement("div");
     box.classList.add("box", "box--default");
     sketchpad.append(box);
 }
+
+//when another color is picked
+colorRange.addEventListener("input", function(){
+    //update the global variable: current color
+    currentColor = colorRange.value;
+});
+
+
